9536814d74b413b54d039bbd7a8c901f33c4dac7
Manh Cuong <cuong@radioactive.sg>
2021-08-05 22:31:20 +0800

    update scripit

--- resources/views/layouts/homeScript.blade.php
+++ resources/views/layouts/homeScript.blade.php
@@ -4,7 +4,6 @@
 <script src="{{url('assets/js/main.js')}}"></script>
 <script src="{{url('assets/js/player.js')}}"></script>
 <script src="{{url('assets/js/bootstrap.bundle.min.js')}}"></script>
-<script src="{{url('assets/js/bootstrap.min.js')}}"></script>
 <script src="{{url('assets/owlcarousel/owl.carousel.min.js')}}"></script>
 <script src="https://kit.fontawesome.com/16985780f3.js" crossorigin="anonymous"></script>
 @yield('scripts')
