8e00dea06b481f8a5c35b71e1c16898b7326cf17
Manh Cuong <cuong@radioactive.sg>
2021-08-10 10:19:23 +0800

    update vnpay return

--- resources/views/template/vnpayReturn.blade.php
+++ resources/views/template/vnpayReturn.blade.php
@@ -4,73 +4,84 @@
 @endsection
 
 @section('content')
-<!--Category-banner-start-->
-<div class="category-banner">
-  <div class="text-block">
-    <p class="category">Trang</p>
-    <p class="eco">Thanh toán</p>
+<div class="success-purchase-body fixed-top" style="visibility: visible; opacity: 1">
+  <div class="logo-thecup">
+    <img src="/thecup/resources/img/logo-login-register.svg" alt="">
   </div>
-</div>
-<!--Contact-start-->
-<div class="contact-holder">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-7 right" style="border:none; width: 60%">
-        <p class="title">
-          Thông tin thanh toán
-        </p>
-        <div class="row">
-            <div class="col-md-12">
-                <p class="input-name">Mã đơn hàng</p>
-                <div class="input-group">
-                    <input class="form-control" id="amount"
-                            name="amount" type="number" value="{{$payment->txn_ref}}" readonly/>
-                </div>
-                <p class="input-name">Số tiền</p>
-                <div class="input-group">
-                    <input class="form-control" id="amount"
-                            name="amount"  value="<?=number_format($payment->amount/100). " VND"?>" readonly/>
-                </div>
+  <div class="register-holder">
+    <div class="upper failed">
+      @if ($payment->vnp_checkhash && $payment->response_code == "00")
+        <img class="success-purchase-img" src="/thecup/resources/img/success-ic.svg" alt="">
+        <p class="reg-title success">Thanh toán thành công</p>
+      @else
+        <img class="failed-purchase-img" src="/thecup/resources/img/failed-ic.svg" alt="">
+         <p class="reg-title failed">Thanh toán thất bại</p>
+      @endif
+      <p class="purchase-text">Số tiền thanh toán</p>
+      <p class="purchase-amount"><?=number_format($payment->amount/100). " VND"?></p>
+
+      <div class="custom-divider"></div>
+      <div class="custom-decor-left"></div>
+      <div class="custom-decor-right"></div>
+
+      <div class="purchase-information-holder">
+        <div class="line">
+          <div class="row">
+            <div class="col-md-5">
+              <p class="title">Mã đơn hàng</p>
             </div>
-            <div class="col-md-12">
-                <p class="input-name">Nội dung thanh toán</p>
-                <div class="input-group"  style="position: relative">
-                    <textarea class="form-control" cols="20" id="order_desc" name="order_desc" rows="2" readonly><?php echo $payment->order_info ?>
-                    </textarea>
-                </div>
-                <p class="input-name">Thời gian thanh toán</p>
-                <div class="input-group" style="position: relative">
-                    <input class="form-control" id="amount"
-                            name="amount"  value="<?php echo $payment->paydate  ?>" readonly/>
-                </div>
+            <div class="col-md-7">
+              <p class="value">{{$payment->txn_ref}}</p>
             </div>
-            <div class="col-md-12">
-                <p class="input-name">Mã GD Tại VNPAY</p>
-                <div class="input-group"  style="position: relative">
-                    <input class="form-control" id="amount"
-                            name="amount"  value="<?php echo $payment->transaction_no ?>" readonly/>
-                </div>
-                <p class="input-name">Kết quả giao dịch</p>
-                <div class="input-group" style="position: relative">
-                    <input class="form-control" id="amount"
-                            name="amount"  value="<?php
-                                            if ($payment->vnp_checkhash) {
-                                                if ($payment->response_code == '00') {
-                                                    echo "GD Thanh cong";
-                                                } else {
-                                                    echo "GD Khong thanh cong";
-                                                }
-                                            } else {
-                                                echo "Chu ky khong hop le";
-                                            }
-                                            ?>" readonly/>
-                </div>
-            </div>
+          </div>
         </div>
-        <div class="margin-top"></div>
+        <div class="line">
+          <div class="row">
+            <div class="col-md-5">
+              <p class="title">Thời gian thành toán</p>
+            </div>
+            <div class="col-md-7">
+              <p class="value">{{$payment->paydate}}</p>
+            </div>
+          </div>
+        </div>
+        <div class="line">
+          <div class="row">
+            <div class="col-md-5">
+              <p class="title">Nội dung thanh toán</p>
+            </div>
+            <div class="col-md-7">
+              <p class="value"><?= $payment->order_info ?></p>
+            </div>
+          </div>
+        </div>
+        <div class="line">
+          <div class="row">
+            <div class="col-md-5">
+              <p class="title">Mã giao dịch VN Pay</p>
+            </div>
+            <div class="col-md-7">
+              <p class="value">{{$payment->transaction_no}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="oauth-button">
         <a href="{{url('user/profile')}}">
-            <button type="submit" class="btn btn-primary">Đồng ý</button>
+          <button class="btn btn-primary btn-accept-purchase">
+            <span>Về trang chủ</span>
+          </button>
         </a>
+      </div>
+
+    </div>
+    <div class="lower">
+      <div class="login-block">
+        <span class="text">Mọi vấn đề phát sinh vui lòng liên hệ</span>
+        <button class="btn btn-purchase-contact">
+          <span class="login">1800 9990</span>
+        </button>
       </div>
     </div>
   </div>
