b4ba2dc774527a215b86fe4173933d4c3cab24fb
Manh Cuong <cuong@radioactive.sg>
2021-08-24 19:59:23 +0700

    update restrict page

--- resources/views/layouts/restrict.blade.php
+++ resources/views/layouts/restrict.blade.php
@@ -3,12 +3,22 @@
     <div class="restrict-layer-text fixed-bottom transition">
         <div class="wrapper">
             <p class="text1">Bài viết bị giới hạn</p>
+            @guest
             <p class="text2">Vui lòng Đăng nhập hoặc Đăng ký để tiếp tục</p>
             <div class="button-holder">
                 <button class="btn btn-primary button-reg" id="registerBtn">Đăng ký</button>
                 <span>Hoặc</span>
                 <button class="btn btn-primary button-log" id="loginBtn">Đăng nhập</button>
             </div>
+            @else
+            <p class="text2">Vui lòng nâp cấp gói dịch vụ để đọc bài viết này</p>
+            <div class="button-holder">
+                <button class="btn btn-primary button-reg">
+                    <a href="{{ url('user/profile') }}">Thông tin tài khoản
+                    </a>
+                </button>
+            </div>
+            @endguest
         </div>
     </div>
 </div>
